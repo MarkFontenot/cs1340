f606fe1fd3da359cd0b09f68bb33bc6a03e59430
Xinyi Ding <dxywill@gmail.com>
2020-02-18 11:23:59 -0600

    week5 lec1

---
+++ code/quiz.py
@@ -0,0 +1,19 @@
+# x = ['ab', 'cd']
+# for i in x:
+#     i.upper()
+# print(x)
+
+# print("hello" + str(1) + str(2) + str(3))
+
+
+# print([i.lower() for i in "HELLO"])
+
+# a = int(input("first number"))
+# b = int(input("second number"))
+# print(a * b)
+
+def build_person(first_name, last_name):
+    return  {
+        "first": first_name,
+        "last" : last_name
+    }
---
+++ code/week5/file_handling.py
@@ -0,0 +1,3 @@
+f = open("cs1340.txt", "r")
+content = f.read(10)
+print(content)
